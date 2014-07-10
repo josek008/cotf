788ee3a2181cde076127f99134368e6f1e6b2407
josek008 <jcarmonaguerra@hotmail.com>
2014-07-09 19:10:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cotf::Application.config.secret_key_base = 'b7e480e1567d9c5f7663c32c52c5090a575535f24cb27c6c836ab03c473fb89cc4116866cac1e7f973b4c6e6bb2e5203bc55d1498c9df6e16283d617ec4481d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cotf::Application.config.session_store :cookie_store, key: '_cotf_session'
